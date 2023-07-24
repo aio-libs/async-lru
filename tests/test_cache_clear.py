b5e9c62fd4d8ede2418f788f7ba279ec61bf06de
Josef Kvita <kvita@remak.cz>
2023-07-24 09:41:18 +0200

    Add a test for clearing the cache before the task is finished

--- tests/test_cache_clear.py
+++ tests/test_cache_clear.py
@@ -18,3 +18,22 @@ async def test_cache_clear(check_lru: Callable[..., None]) -> None:
     coro.cache_clear()
 
     check_lru(coro, hits=0, misses=0, cache=0, tasks=0)
+
+
+async def test_cache_clear_before_task_finished(
+        check_lru: Callable[..., None]
+) -> None:
+    @alru_cache()
+    async def coro(val: int) -> int:
+        return val
+
+    async def wrapper(val: int) -> int:
+        coro.cache_clear()
+        return await coro(val=val)
+
+    inputs = [1, 2, 3]
+    coros = [wrapper(v) for v in inputs]
+    ret = await asyncio.gather(*coros)
+    assert ret == inputs
+
+    check_lru(coro, hits=0, misses=1, cache=1, tasks=0)
