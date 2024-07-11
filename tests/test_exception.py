bf10fc39b5ba4e4e1752a0f294354002dee32a65
Ivan Lakovic <laky55555@gmail.com>
2024-07-11 22:56:05 +0100

    Test to show memory leak

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -1,4 +1,5 @@
 import asyncio
+import gc
 from typing import Callable
 
 import pytest
@@ -25,3 +26,25 @@ async def test_alru_exception(check_lru: Callable[..., None]) -> None:
         await coro(1)
 
     check_lru(coro, hits=2, misses=2, cache=1, tasks=0)
+
+
+async def test_alru_exception_reference_cleanup(check_lru: Callable[..., None]) -> None:
+    class CustomClass: ...
+
+    @alru_cache()
+    async def coro(val: int) -> None:
+        leaky = CustomClass()
+        1 / 0
+
+    coros = [coro(v) for v in range(1000)]
+
+    await asyncio.gather(*coros, return_exceptions=True)
+
+    check_lru(coro, hits=0, misses=1000, cache=128, tasks=0)
+
+    await asyncio.sleep(0.00001)
+    gc.collect()
+
+    assert (
+        len([obj for obj in gc.get_objects() if isinstance(obj, CustomClass)]) == 128
+    ), "Only objects in the cache should be left in memory."
