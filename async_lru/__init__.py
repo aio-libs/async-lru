095098846d5cd6eafcdefb6087f810b3a2caaf7d
Sam Bull <git@sambull.org>
2025-02-02 22:26:07 +0000

    Formatting

--- async_lru/__init__.py
+++ async_lru/__init__.py
@@ -223,9 +223,7 @@ class _LRUCacheWrapper(Generic[_P, _R]):
         return await asyncio.shield(fut)
 
     @overload
-    def __get__(
-        self, instance: _T, owner: None
-    ) -> Self:
+    def __get__(self, instance: _T, owner: None) -> Self:
         ...
 
     @overload
