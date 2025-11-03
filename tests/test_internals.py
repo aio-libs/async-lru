9fafc6e4b194556fc3dd12783183a7fcb2d2e6f4
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-11-03 15:57:18 -0400

    Update test_internals.py

--- tests/test_internals.py
+++ tests/test_internals.py
@@ -15,7 +15,7 @@ async def test_done_callback_cancelled() -> None:
 
     key = 1
 
-    task.add_done_callback(partial(wrapped._task_done_callback, fut, key))
+    task.add_done_callback(wrapped._get_done_callback(fut, key))
     wrapped._LRUCacheWrapper__tasks.add(task)  # type: ignore[attr-defined]
 
     task.cancel()
@@ -33,7 +33,7 @@ async def test_done_callback_exception() -> None:
 
     key = 1
 
-    task.add_done_callback(partial(wrapped._task_done_callback, fut, key))
+    task.add_done_callback(wrapped._get_done_callback(fut, key))
     wrapped._LRUCacheWrapper__tasks.add(task)  # type: ignore[attr-defined]
 
     exc = ZeroDivisionError()
@@ -59,7 +59,7 @@ async def test_done_callback() -> None:
     key = 1
     fut = loop.create_future()
 
-    task.add_done_callback(partial(wrapped._task_done_callback, fut, key))
+    task.add_done_callback(wrapped._get_done_callback(fut, key))
     wrapped._LRUCacheWrapper__tasks.add(task)  # type: ignore[attr-defined]
 
     task.set_result(1)
